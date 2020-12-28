0a79e985048a59571ca3cb3373a312a21682cf8b
jasonouyang8000 <jianjun.ouyang@lacity.org>
2020-12-28 13:34:40 -0800

    changes

--- script.js
+++ script.js
@@ -6,6 +6,17 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
+var checkStatus = function checkStatus(response) {
+    if (response.ok) {
+        return response;
+    }
+    throw new Error('Request was either 404 or 500');
+};
+
+var json = function json(response) {
+    return response.json();
+};
+
 var NotesMenu = function NotesMenu(props) {
     var categoryChange = props.categoryChange,
         categories = props.categories;
@@ -14,7 +25,10 @@ var NotesMenu = function NotesMenu(props) {
     for (var prop in categories) {
         buttons.push(React.createElement(
             "button",
-            { key: prop, onClick: categoryChange, className: "btn btn-outline-dark me-2 " + categories[prop] },
+            {
+                key: prop,
+                onClick: categoryChange,
+                className: "btn btn-outline-dark me-2 " + categories[prop] },
             prop
         ));
     }
@@ -28,7 +42,7 @@ var NotesMenu = function NotesMenu(props) {
 var CreateNote = function CreateNote(props) {
     return React.createElement(
         "div",
-        { className: "col-3" },
+        { className: "col-12 col-md-4 col-lg-3 mb-4" },
         React.createElement(
             "form",
             { onSubmit: props.handleSubmit, className: "frm-create-note d-flex flex-column" },
@@ -39,10 +53,67 @@ var CreateNote = function CreateNote(props) {
 };
 
 var ShowNotes = function ShowNotes(props) {
+    var notes = props.notes,
+        deleteNote = props.deleteNote,
+        category = props.category,
+        markNote = props.markNote;
+
+
+    switch (category) {
+        case 'Completed':
+            notes = notes.filter(function (note) {
+                return note.completed;
+            });
+            break;
+        case 'Incomepleted':
+            notes = notes.filter(function (note) {
+                return !note.completed;
+            });
+            break;
+    }
+
+    notes = notes.map(function (note) {
+        var content = note.content,
+            completed = note.completed,
+            id = note.id;
+
+        return React.createElement(
+            "div",
+            { key: id, className: "col-12 col-md-4 col-lg-3 mb-4" },
+            React.createElement(
+                "div",
+                { className: "note" },
+                React.createElement(
+                    "p",
+                    { className: completed ? 'text-decoration-line-through' : '' },
+                    content
+                ),
+                React.createElement(
+                    "div",
+                    { className: "btn-group mt-3" },
+                    React.createElement(
+                        "button",
+                        { onClick: function onClick(e) {
+                                return deleteNote(id);
+                            }, className: "btn btn-outline-dark" },
+                        "Delete"
+                    ),
+                    React.createElement(
+                        "button",
+                        { onClick: function onClick(e) {
+                                return markNote(id, completed);
+                            }, className: "btn btn-outline-dark" },
+                        React.createElement("i", { className: "fas " + (completed ? 'fa-times red' : 'fa-check green') })
+                    )
+                )
+            )
+        );
+    });
+
     return React.createElement(
-        "div",
-        { className: "div-col-3" },
-        React.createElement("div", { className: "post-sticks" })
+        React.Fragment,
+        null,
+        notes
     );
 };
 
@@ -66,11 +137,20 @@ var ToDoList = function (_React$Component) {
 
         _this.Apikey = 248;
         _this.handleChange = _this.handleChange.bind(_this);
+        _this.handleSubmit = _this.handleSubmit.bind(_this);
         _this.categoryChange = _this.categoryChange.bind(_this);
+        _this.deleteNote = _this.deleteNote.bind(_this);
+        _this.getCurrentActive = _this.getCurrentActive.bind(_this);
+        _this.markNote = _this.markNote.bind(_this);
         return _this;
     }
 
     _createClass(ToDoList, [{
+        key: "handleError",
+        value: function handleError(error) {
+            console.log(error);
+        }
+    }, {
         key: "getCurrentActive",
         value: function getCurrentActive(object) {
             for (var prop in object) {
@@ -100,9 +180,71 @@ var ToDoList = function (_React$Component) {
             });
         }
     }, {
+        key: "markNote",
+        value: function markNote(id, completed) {
+            var _this2 = this;
+
+            fetch("https://altcademy-to-do-list-api.herokuapp.com/tasks/" + id + "/mark_" + (completed ? 'active' : 'complete') + "?api_key=248", {
+                method: "PUT",
+                mode: "cors"
+            }).then(checkStatus).then(json).then(function (data) {
+                return _this2.getNotes();
+            }).catch(this.handleError);
+        }
+    }, {
+        key: "getNotes",
+        value: function getNotes() {
+            var _this3 = this;
+
+            fetch('https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=248').then(checkStatus).then(json).then(function (data) {
+                return _this3.setState({ notes: data.tasks });
+            }).catch(this.handleError);
+        }
+    }, {
         key: "handleSubmit",
         value: function handleSubmit(e) {
+            var _this4 = this;
+
             e.preventDefault();
+            var currentNote = this.state.currentNote;
+
+            if (!currentNote) {
+                return;
+            }
+
+            fetch('https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=248', {
+                method: 'POST',
+                mode: 'cors',
+                credentials: 'same-origin',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    task: {
+                        content: currentNote
+                    }
+                })
+            }).then(checkStatus).then(json).then(function (data) {
+                _this4.getNotes();
+                _this4.setState({ currentNote: '' });
+            }).catch(this.handleError);
+        }
+    }, {
+        key: "deleteNote",
+        value: function deleteNote(id) {
+            var _this5 = this;
+
+            fetch("https://altcademy-to-do-list-api.herokuapp.com/tasks/" + id + "?api_key=248", {
+                method: 'DELETE',
+                mode: 'cors'
+            }).then(checkStatus).then(json).then(function (data) {
+                _this5.getNotes();
+            }).catch(this.handleError);
+        }
+    }, {
+        key: "componentDidMount",
+        value: function componentDidMount() {
+            this.getNotes();
         }
     }, {
         key: "render",
@@ -113,7 +255,7 @@ var ToDoList = function (_React$Component) {
                 React.createElement(
                     "h1",
                     { className: "text-center" },
-                    " To Do List "
+                    "To Do List"
                 ),
                 React.createElement(
                     "div",
@@ -121,9 +263,21 @@ var ToDoList = function (_React$Component) {
                     React.createElement(
                         "div",
                         { className: "row" },
-                        React.createElement(NotesMenu, { categories: this.state.categories, categoryChange: this.categoryChange }),
-                        React.createElement(CreateNote, { handleChange: this.handleChange, handleSubmit: this.handleSubmit, currentNote: this.state.currentNote }),
-                        React.createElement(ShowNotes, null)
+                        React.createElement(NotesMenu, {
+                            categories: this.state.categories,
+                            categoryChange: this.categoryChange
+                        }),
+                        React.createElement(CreateNote, {
+                            handleChange: this.handleChange,
+                            handleSubmit: this.handleSubmit,
+                            currentNote: this.state.currentNote
+                        }),
+                        React.createElement(ShowNotes, {
+                            notes: this.state.notes,
+                            deleteNote: this.deleteNote,
+                            category: this.getCurrentActive(this.state.categories),
+                            markNote: this.markNote
+                        })
                     )
                 )
             );
--- src/script.js
+++ src/script.js
@@ -1,8 +1,23 @@
+const checkStatus = response => {
+    if (response.ok) {
+        return response;
+    }
+    throw new Error('Request was either 404 or 500');
+}
+
+const json = response => response.json();
+
+
 const NotesMenu = (props) => {
     const {categoryChange,categories} = props;
     const buttons = [];
     for (let prop in categories) {
-        buttons.push(<button key={prop} onClick={categoryChange} className={`btn btn-outline-dark me-2 ${categories[prop]}`} >{prop}</button>);
+        buttons.push(<button 
+                    key={prop} 
+                    onClick={categoryChange} 
+                    className={`btn btn-outline-dark me-2 ${categories[prop]}`}>
+                    {prop}
+                    </button>);
     }
     return (
         <div className="d-flex mb-5">
@@ -13,7 +28,7 @@ const NotesMenu = (props) => {
 
 const CreateNote = (props) => {
     return(
-        <div className="col-3">
+        <div className="col-12 col-md-4 col-lg-3 mb-4">
             <form onSubmit={props.handleSubmit} className="frm-create-note d-flex flex-column">
                 <textarea onChange={props.handleChange} rows="5" value={props.currentNote}>
                     
@@ -27,13 +42,38 @@ const CreateNote = (props) => {
 }
 
 const ShowNotes = (props) => {
-    return (
-        <div className="div-col-3">
-            <div className="post-sticks">
+    let {notes,deleteNote,category,markNote} = props;
+    
+    switch(category) {
+        case 'Completed':
+            notes = notes.filter(note => (note.completed));
+            break;
+        case 'Incomepleted':
+            notes = notes.filter(note => !note.completed);
+            break;
+    }
 
-            
+    notes = notes.map(note => {
+        const {content,completed,id} = note;
+        return (
+            <div key={id} className="col-12 col-md-4 col-lg-3 mb-4">
+                <div className="note">
+                    <p className={completed?'text-decoration-line-through':''} >{content}</p>
+                    <div className="btn-group mt-3">
+                        <button onClick={e => deleteNote(id)} className="btn btn-outline-dark">Delete</button>
+                        <button  onClick={e => markNote(id,completed)} className="btn btn-outline-dark"><i className={`fas ${completed?'fa-times red':'fa-check green'}`}></i></button>
+                    </div>
+                </div>
             </div>
-        </div>
+        );
+    });
+
+
+
+    return (
+        <React.Fragment>
+          {notes}
+        </React.Fragment>
     );
 }
 
@@ -50,11 +90,17 @@ class ToDoList extends React.Component {
             }
         }
 
-      
-
         this.Apikey = 248;
         this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
         this.categoryChange = this.categoryChange.bind(this);
+        this.deleteNote = this.deleteNote.bind(this);
+        this.getCurrentActive = this.getCurrentActive.bind(this);
+        this.markNote = this.markNote.bind(this);
+    }
+
+    handleError(error) {
+        console.log(error);
     }
     
     getCurrentActive(object) {
@@ -83,19 +129,97 @@ class ToDoList extends React.Component {
 
     }
 
-    handleSubmit(e) {
-        e.preventDefault();
+    markNote(id,completed) {
+        fetch(`https://altcademy-to-do-list-api.herokuapp.com/tasks/${id}/mark_${completed?'active':'complete'}?api_key=248`,{
+            method: "PUT",
+            mode: "cors",
+        })
+        .then(checkStatus)
+            .then(json)
+                .then(data => this.getNotes())
+                        .catch(this.handleError);
     }
 
+    getNotes() {
+        fetch('https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=248')
+        .then(checkStatus)
+            .then(json)
+                .then(data => this.setState({notes:data.tasks}))
+                        .catch(this.handleError);
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        const {currentNote} = this.state;
+    if (!currentNote) {
+        return;
+    }
+
+    
+
+  
+
+    fetch('https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=248',{
+        method: 'POST',
+        mode: 'cors',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            task: {
+                content: currentNote
+            }
+        })
+    }).then(checkStatus)
+            .then(json)
+                .then(data => {
+                    this.getNotes();
+                    this.setState({currentNote:''});
+                })
+                    .catch(this.handleError);
+       
+    }
+
+    deleteNote(id) {
+        fetch(`https://altcademy-to-do-list-api.herokuapp.com/tasks/${id}?api_key=248`,{
+            method:'DELETE',
+            mode:'cors',
+        })
+            .then(checkStatus)
+                .then(json)
+                    .then(data => {
+                       this.getNotes();
+                    })
+                        .catch(this.handleError);
+   
+    }
+
+    componentDidMount() {
+        this.getNotes();
+    }
+    
     render() {
         return(
             <div>
-                <h1 className="text-center"> To Do List </h1>
+                <h1 className="text-center">To Do List</h1>
                 <div className="container">
                     <div className="row">
-                        <NotesMenu categories={this.state.categories} categoryChange={this.categoryChange} />
-                        <CreateNote handleChange={this.handleChange} handleSubmit={this.handleSubmit} currentNote={this.state.currentNote} />
-                        <ShowNotes />
+                        <NotesMenu 
+                        categories={this.state.categories}
+                        categoryChange={this.categoryChange}
+                          />
+                        <CreateNote 
+                        handleChange={this.handleChange} 
+                        handleSubmit={this.handleSubmit} 
+                        currentNote={this.state.currentNote} 
+                        />
+                        <ShowNotes 
+                        notes={this.state.notes} 
+                        deleteNote={this.deleteNote} 
+                        category={this.getCurrentActive(this.state.categories)} 
+                        markNote={this.markNote} 
+                        />
                     </div>
                 </div>
             </div>
